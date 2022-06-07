133a3e8603c6ac1b5c6bf3ac9984616703f0188f
TheOmegaCarrot <theomegacarrot@gmail.com>
2022-06-07 18:11:50 -0400

    fix documentation

--- inc/utils/algorithm.hpp
+++ inc/utils/algorithm.hpp
@@ -7,19 +7,15 @@ namespace ehanc {
 	/**
 	 * @brief Applies `func` to each adjacent pair of elements.
 	 *
+	 * Example: Range is: {1, 2, 3}
+	 *
+	 * Calls to `func` will be: `func(1, 2)`, `func(2, 3)`
+	 *
 	 * @param begin Iterator to the beginning of the range.
 	 *
 	 * @param end Iterator to the end of the range.
 	 *
-	 * @param func Binary function to apply to each pair.
-	 *
-	 * @code {
-	 * std::array<int,3> example = {1, 2, 3};
-	 * for_each_pair(example.cbegin(), example.cend(),
-	 * 		[](const int i, const int j){
-	 *			std::cout << i + j << '\t';
-	 * 		}); // will print "3		5"
-	 * @}
+	 * @param func Binary function to apply to each adjacent pair.
 	 */
 	/* }}} */
 	template<typename Itr, typename BinaryFunc>

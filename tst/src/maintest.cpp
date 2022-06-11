b0e13796c3392213b38944a4514c3d81d9620c82
TheOmegaCarrot <theomegacarrot@gmail.com>
2022-06-11 19:40:24 -0400

    prettier test output

--- tst/src/maintest.cpp
+++ tst/src/maintest.cpp
@@ -1,13 +1,24 @@
-#include "test_utils.h"
+#include <iostream>
 
+#include "test_utils.h"
+#include "utils/term_colors.h"
 #include "test_algorithm.h"
 #include "test_lazy.h"
+
+constexpr static const std::string_view HEADER_COLOR = ehanc::FG_RED;
 
 int main()
 {
 
+	std::cout << HEADER_COLOR << "Algorithm:" << ehanc::RESET << '\n';
 	test_algorithm();
+
+	std::cout << '\n' << '\n';
+
+	std::cout << HEADER_COLOR << "Lazy:" << ehanc::RESET << '\n';
 	test_lazy();
+
+	std::cout << '\n' << '\n';
 
 	return 0;
 }

97678dda008b737a64ac745ddd61825cc6f43f63
TheOmegaCarrot <theomegacarrot@gmail.com>
2022-06-12 17:13:09 -0400

    for_each_all_n and all_pass

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
 
-#include "utils/algorithm.hpp"
-
 int main( [[maybe_unused]] const int argc,
 		[[maybe_unused]] const char *const *const argv )
 {
-
-	std::array one{1, 2, 3, 4};
-	std::array two(one);
-	std::array three{4, 3, 2, 1};
-
-	ehanc::for_n_all([](const auto... args) {
-				std::cout << (... + args) << '\n';
-			}, 4, one.cbegin(), two.cbegin(), three.cbegin());
-
-	std::cout << '\n' << '\n';
-
-	ehanc::for_n_all([](const int a, const int b, const int c) {
-				std::cout << a << ' ' << b << ' ' << c << ' ' << '\n';
-			}, 4, one.cbegin(), two.cbegin(), three.cbegin());
 
 	return 0;
 }

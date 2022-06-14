489408a7321e78aed8e34df3f15832f54ad29be0
TheOmegaCarrot <theomegacarrot@gmail.com>
2022-06-14 08:23:10 -0400

    more in main demo for lazy

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main( [[maybe_unused]] const int argc,
 		[[maybe_unused]] const char *const *const argv )
 {
 
-	ehanc::lazy<int> test([i = loud(7)](){return i();});
+	ehanc::lazy<int> test([i{loud(7)}](){return i();});
 
 	std::cout << test << '\n';
 	std::cout << test << '\n';
@@ -45,6 +45,16 @@ int main( [[maybe_unused]] const int argc,
 
 	std::cout << test2 << '\n';
 	std::cout << test2 << '\n';
+
+	auto test3{ehanc::make_lazy([i{loud(3)}](){return i();})};
+
+	std::cout << test3 << '\n';
+	std::cout << test3 << '\n';
+
+	auto test4{ehanc::make_lazy(loud(6))};
+
+	std::cout << test4 << '\n';
+	std::cout << test4 << '\n';
 
 	return 0;
 }

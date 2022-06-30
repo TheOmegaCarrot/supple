9fcfe6ad9f23ddcd03dbdd08846598a7a167d7fc
TheOmegaCarrot <theomegacarrot@gmail.com>
2022-06-29 22:16:04 -0400

    organize demo in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,15 @@
 
 /// @cond
 
+void lazy_demo();
+
+int main([[maybe_unused]] const int argc,
+         [[maybe_unused]] const char* const* const argv)
+{
+
+  return 0;
+}
+
 struct loud {
 
   int m_val;
@@ -47,43 +56,35 @@ auto printer(const T& print)
   std::cout << print << '\n';
 }
 
-int main([[maybe_unused]] const int argc,
-         [[maybe_unused]] const char* const* const argv)
+void lazy_demo()
 {
+  ehanc::lazy<int> test([i{loud(7)}]() { return i(); });
 
-  /* ehanc::lazy<int> test([i{loud(7)}]() { return i(); }); */
+  std::cout << test << '\n';
+  std::cout << test << '\n';
 
-  /* std::cout << test << '\n'; */
-  /* std::cout << test << '\n'; */
+  ehanc::lazy<int> test2(loud(9));
 
-  /* ehanc::lazy<int> test2(loud(9)); */
+  std::cout << test2 << '\n';
+  std::cout << test2 << '\n';
 
-  /* std::cout << test2 << '\n'; */
-  /* std::cout << test2 << '\n'; */
+  auto test3{ehanc::make_lazy([i{loud(3)}]() { return i(); })};
 
-  /* auto test3{ehanc::make_lazy([i{loud(3)}]() { return i(); })}; */
+  std::cout << test3 << '\n';
+  std::cout << test3 << '\n';
 
-  /* std::cout << test3 << '\n'; */
-  /* std::cout << test3 << '\n'; */
+  auto test4{ehanc::make_lazy(loud(6))};
 
-  /* auto test4{ehanc::make_lazy(loud(6))}; */
+  std::cout << test4 << '\n';
+  std::cout << test4 << '\n';
 
-  /* std::cout << test4 << '\n'; */
-  /* std::cout << test4 << '\n'; */
+  auto test5{ehanc::make_lazy([i{loud(14)}]() { return i(); })};
 
-  /* auto test5{ehanc::make_lazy([i{loud(14)}]() { return i(); })}; */
+  printer(test5);
+  printer(test5);
 
-  /* printer(test5); */
-  /* printer(test5); */
+  ehanc::lazy deduced([i{loud(2)}]() { return i(); });
 
-  /* ehanc::lazy deduced([i{loud(2)}]() { return i(); }); */
-
-  /* std::cout << deduced << '\n'; */
-  /* std::cout << deduced << '\n'; */
-
-  /* for ( int i : ehanc::sequence(0, 10) ) { */
-  /*   std::cout << i << '\n'; */
-  /* } */
-
-  return 0;
+  std::cout << deduced << '\n';
+  std::cout << deduced << '\n';
 }

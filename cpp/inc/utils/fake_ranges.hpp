6813db26e0c60e4fadd0bf1c1d5c01a8ee785410
theomegacarrot <theomegacarrot@gmail.com>
2022-11-12 23:38:43 -0500

    for_each{,n}, count, noexcepting

--- cpp/inc/utils/fake_ranges.hpp
+++ cpp/inc/utils/fake_ranges.hpp
@@ -10,30 +10,57 @@
 namespace ehanc::fr {
 
 template <typename Container, typename Pred>
-auto all_of(Container&& container, Pred&& pred) -> bool
+auto all_of(Container&& container, Pred&& pred) noexcept(
+    noexcept(std::all_of(std::begin(container), std::end(container),
+                         std::forward<Pred>(pred)))) -> bool
 {
-  auto begin {std::begin(container)};
-  auto end {std::end(container)};
-
-  return std::all_of(begin, end, std::forward<Pred>(pred));
+  return std::all_of(std::begin(container), std::end(container),
+                     std::forward<Pred>(pred));
 }
 
 template <typename Container, typename Pred>
-auto any_of(Container&& container, Pred&& pred) -> bool
+auto any_of(Container&& container, Pred&& pred) noexcept(
+    noexcept(std::any_of(std::begin(container), std::end(container),
+                         std::forward<Pred>(pred)))) -> bool
 {
-  auto begin {std::begin(container)};
-  auto end {std::end(container)};
-
-  return std::any_of(begin, end, std::forward<Pred>(pred));
+  return std::any_of(std::begin(container), std::end(container),
+                     std::forward<Pred>(pred));
 }
 
 template <typename Container, typename Pred>
-auto none_of(Container&& container, Pred&& pred) -> bool
+auto none_of(Container&& container, Pred&& pred) noexcept(
+    noexcept(std::none_of(std::begin(container), std::end(container),
+                          std::forward<Pred>(pred)))) -> bool
 {
-  auto begin {std::begin(container)};
-  auto end {std::end(container)};
+  return std::none_of(std::begin(container), std::end(container),
+                      std::forward<Pred>(pred));
+}
 
-  return std::none_of(begin, end, std::forward<Pred>(pred));
+template <typename Container, typename Func>
+auto for_each(Container&& container, Func&& func) noexcept(
+    noexcept(std::for_each(std::begin(container), std::end(container),
+                           std::forward<Func>(func)))) -> Func
+{
+  return std::for_each(std::begin(container), std::end(container),
+                       std::forward<Func>(func));
+}
+
+template <typename Container, typename Size, typename Func>
+auto for_each_n(Container&& container, Size n, Func&& func) noexcept(
+    noexcept(std::for_each_n(std::begin(container), std::end(container), n,
+                             std::forward<Func>(func)))) -> Func
+{
+  return std::for_each_n(std::begin(container), std::end(container), n,
+                         std::forward<Func>(func));
+}
+
+template <typename Container, typename T>
+auto count(Container&& container, const T& value) noexcept(noexcept(
+    std::count(std::begin(container), std::end(container), value))) ->
+    typename std::iterator_traits<
+        decltype(std::begin(container))>::difference_type
+{
+  return std::count(std::begin(container), std::end(container), value);
 }
 
 } // namespace ehanc::fr
--- cpp/tst/src/utils/test_fake_ranges.cpp
+++ cpp/tst/src/utils/test_fake_ranges.cpp
@@ -60,9 +60,26 @@ static auto test_none_of() -> ehanc::test
   return results;
 }
 
+static auto test_count() -> ehanc::test
+{
+  ehanc::test results;
+
+  const std::vector cvec {2, 3, 4, 5, 5};
+
+  auto count {ehanc::fr::count(cvec, 5)};
+  auto correct {static_cast<decltype(count)>(2)};
+
+  results.add_case(count, correct);
+
+  return results;
+}
+
 void test_fake_ranges()
 {
   ehanc::run_test("all_of", &test_all_of);
   ehanc::run_test("any_of", &test_any_of);
   ehanc::run_test("none_of", &test_none_of);
+  /* ehanc::run_test("for_each", &test_for_each); */
+  /* ehanc::run_test("for_each_n", &test_for_each_n); */
+  ehanc::run_test("count", &test_count);
 }

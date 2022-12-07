7b042337b63cd64675ac369c5bb4b28a9743b3d3
theomegacarrot <theomegacarrot@gmail.com>
2022-12-06 21:33:44 -0500

    add tl::any_of

--- cpp/inc/supl/type_list.hpp
+++ cpp/inc/supl/type_list.hpp
@@ -206,9 +206,22 @@ template <template <typename...> typename LIST,
 struct all_of<LIST<>, PRED> : std::true_type {};
 
 template <typename LIST, template <typename> typename PRED>
-constexpr inline bool all_of_v = all_of<LIST, PRED>::value;
+constexpr inline bool all_of_v = ::supl::tl::all_of<LIST, PRED>::value;
 
 ///////////////////////////////////////////// any_of
+
+template <typename LIST, template <typename> typename PRED>
+struct any_of
+    : std::conditional_t<
+          PRED<::supl::tl::front_t<LIST>>::value, std::true_type,
+          ::supl::tl::any_of<::supl::tl::pop_front_t<LIST>, PRED>> {};
+
+template <template <typename...> typename LIST,
+          template <typename> typename PRED>
+struct any_of<LIST<>, PRED> : std::false_type {};
+
+template <typename LIST, template <typename> typename PRED>
+constexpr inline bool any_of_v = ::supl::tl::any_of<LIST, PRED>::value;
 
 ///////////////////////////////////////////// none_of
 

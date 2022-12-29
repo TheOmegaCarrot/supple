cf3d280721c237b7afb4bcdd7df45b15682de258
theomegacarrot <theomegacarrot@gmail.com>
2022-12-28 22:54:09 -0500

    move all existing to new target supplementaries::core -- NOT YET TESTED DO NOT USE

--- supplementaries-config.cmake
+++ supplementaries-config.cmake
@@ -1,13 +1,13 @@
 cmake_minimum_required(VERSION 3.22.1)
 
-if(TARGET supplementaries::supplementaries)
+if(TARGET supplementaries::core)
   return()
 endif()
 
-add_library(supplementaries::supplementaries INTERFACE IMPORTED)
-target_include_directories(supplementaries::supplementaries
-                           INTERFACE ${CMAKE_CURRENT_LIST_DIR}/cpp/inc)
-target_compile_features(supplementaries::supplementaries INTERFACE cxx_std_17)
+add_library(supplementaries::core INTERFACE IMPORTED)
+target_include_directories(supplementaries::core
+                           INTERFACE ${CMAKE_CURRENT_LIST_DIR}/cpp/inc/core)
+target_compile_features(supplementaries::core INTERFACE cxx_std_17)
 
 if(NOT DEFINED supplementaries_FIND_QUIETLY)
   message("-- Found supplementaries @ ${CMAKE_CURRENT_LIST_DIR}")

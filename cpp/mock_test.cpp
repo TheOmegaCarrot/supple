cb3edd53641cc84016254dbb1a47f0ae5bec2e1a
theomegacarrot <theomegacarrot@gmail.com>
2022-12-31 22:41:11 -0500

    update mock test setup

--- cpp/mock_test.cpp
+++ cpp/mock_test.cpp
@@ -1,4 +1,5 @@
-supl::test testfunc() {
+supl::test testfunc()
+{
   supl::test results;
 
   results.enforce_exactly_equal(2, 2, "message");
@@ -6,10 +7,14 @@ supl::test testfunc() {
   return results;
 }
 
-supl::section section1() {
+supl::section section1()
+{
   supl::section retval;
   retval.add_test(&testfunc);
   return retval;
 }
 
-int main() { return section1.run() + section2.run(); }
+int main()
+{
+  return section1().run() + section2().run();
+}

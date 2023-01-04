e4a7b8d438c42810b5f09a05b3f566c740911e22
theomegacarrot <theomegacarrot@gmail.com>
2023-01-04 18:38:34 -0500

    adjust cmake-all.sh

--- cpp/scripts/cmake-all.sh
+++ cpp/scripts/cmake-all.sh
@@ -11,8 +11,8 @@
 cd ..
 
 test_compiler() {
-  PREFIX=${1}
-  COMPILER=${2}
+  COMPILER=${1}
+  NAME=${2}
   shift
   shift
   FLAGS=${@}
@@ -22,29 +22,29 @@ test_compiler() {
     cat << EOF
 
 ##############################################################################
-                              ${COMPILER} DEBUG                                     
+                              ${NAME} DEBUG                                     
 ##############################################################################
 
 EOF
 
-    cmake -S cpp -B all/${COMPILER}-debug -DCMAKE_BUILD_TYPE=Debug -DFULL_TESTS=YES ${FLAGS} -DNO_SANITIZERS=YES -DCMAKE_CXX_COMPILER=${PREFIX}/${COMPILER} -DAUTOMATED_BUILD=YES -G Ninja 
+    cmake -S cpp -B all/${NAME}-debug -DCMAKE_BUILD_TYPE=Debug -DFULL_TESTS=YES ${FLAGS} -DNO_SANITIZERS=YES -DCMAKE_CXX_COMPILER=${COMPILER} -DAUTOMATED_BUILD=YES -G Ninja 
     echo
-    cmake --build all/${COMPILER}-debug --parallel $(nproc)
-    cmake --build all/${COMPILER}-debug --parallel $(nproc) --target test
+    cmake --build all/${NAME}-debug --parallel $(nproc)
+    cmake --build all/${NAME}-debug --parallel $(nproc) --target test
 
     cat << EOF
 
 ##############################################################################
-                               ${COMPILER} RELEASE                                   
+                               ${NAME} RELEASE                                   
 ##############################################################################
 
 EOF
 
 
-    cmake -S cpp -B all/${COMPILER}-release -DCMAKE_BUILD_TYPE=Release -DFULL_TESTS=Yes ${FLAGS} -DCMAKE_CXX_COMPILER=${PREFIX}/${COMPILER} -DAUTOMATED_BUILD=YES -G Ninja 
+    cmake -S cpp -B all/${NAME}-release -DCMAKE_BUILD_TYPE=Release -DFULL_TESTS=Yes ${FLAGS} -DCMAKE_CXX_COMPILER=${COMPILER} -DAUTOMATED_BUILD=YES -G Ninja 
     echo
-    cmake --build all/${COMPILER}-release --parallel $(nproc)
-    cmake --build all/${COMPILER}-release --parallel $(nproc) --target test
+    cmake --build all/${NAME}-release --parallel $(nproc)
+    cmake --build all/${NAME}-release --parallel $(nproc) --target test
 
   else
 
@@ -54,16 +54,16 @@ EOF
 
 }
 
-test_compiler /usr/bin g++-9 -DOMIT_23=YES
-test_compiler /usr/bin g++-10 -DOMIT_23=YES
-test_compiler /usr/bin g++-11
-test_compiler /opt/gcc-12/bin g++
-# test_compiler /opt/gcc-trunk/bin g++
-test_compiler /usr/bin clang++-11 -DOMIT_23=YES
-test_compiler /usr/bin clang++-12
-test_compiler /usr/bin clang++-13
-test_compiler /usr/bin clang++-14
-test_compiler /opt/llvm-15/bin clang++
-# test_compiler /opt/llvm-trunk/bin clang++
-test_compiler /opt/intel-2023/oneapi/compiler/2023.0.0/linux/bin/intel64 icc -DOMIT_23=YES
-test_compiler /opt/intel-2023/oneapi/compiler/2023.0.0/linux/bin icpx -DOMIT_23=YES
+test_compiler /usr/bin/g++-9 GCC-9 -DOMIT_23=YES
+test_compiler /usr/bin/g++-10 GCC-10 -DOMIT_23=YES
+test_compiler /usr/bin/g++-11 GCC-11
+test_compiler /opt/gcc-12/bin/g++ GCC-12
+# test_compiler /opt/gcc-trunk/bin/g++ GCC-TRUNK
+test_compiler /usr/bin/clang++-11 CLANG-11 -DOMIT_23=YES
+test_compiler /usr/bin/clang++-12 CLANG-12
+test_compiler /usr/bin/clang++-13 CLANG-13
+test_compiler /usr/bin/clang++-14 CLANG-14
+test_compiler /opt/llvm-15/bin/clang++ CLANG-15
+# test_compiler /opt/llvm-trunk/bin clang++ CLANG-TRUNK
+test_compiler /opt/intel-2023/oneapi/compiler/2023.0.0/linux/bin/intel64/icc ICC -DOMIT_23=YES
+test_compiler /opt/intel-2023/oneapi/compiler/2023.0.0/linux/bin/icpx ICPX -DOMIT_23=YES

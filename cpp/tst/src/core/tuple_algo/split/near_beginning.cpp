5541fd21ee69af65e81e3a816189e6acd60d973e
TheOmegaCarrot <theomegacarrot@gmail.com>
2023-01-23 12:23:50 -0500

    add a test for tuple::split

--- cpp/tst/src/core/tuple_algo/split/near_beginning.cpp
+++ cpp/tst/src/core/tuple_algo/split/near_beginning.cpp
@@ -4,7 +4,16 @@ auto main() -> int
 {
   supl::test_results results;
 
-  // TODO:
+  const std::string a_string {"split"};
+  const std::vector a_vector {1, 2, 3, 4};
+  const std::tuple test_input {3, 3.14, a_vector, true, 'f', a_string};
+
+  const std::tuple expected_first {3};
+  const std::tuple expected_second {3.14, a_vector, true, 'f', a_string};
+  const auto result {supl::tuple::split<1>(test_input)};
+
+  results.enforce_exactly_equal(result.first, expected_first, "First");
+  results.enforce_exactly_equal(result.second, expected_second, "Second");
 
   return results.print_and_return();
 }

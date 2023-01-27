dc119f490ecd30a18f0a21768c7195f0d80fa1cd
TheOmegaCarrot <theomegacarrot@gmail.com>
2023-01-27 11:37:34 -0500

    disassemble test_fake_ranges

---
+++ cpp/tst/src/core/fake_ranges/iota/direct_iterators.cpp
@@ -0,0 +1,23 @@
+#include <supl/fake_ranges.hpp>
+#include <supl/test_results.hpp>
+
+auto main() -> int
+{
+  supl::test_results results;
+
+  const std::array<std::size_t, 10> expected {
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  auto result {[]() {
+    std::array<std::size_t, 10> retval {};
+    std::copy(supl::fr::iota<std::size_t>::iterator {1},
+              supl::fr::iota<std::size_t>::iterator {11},
+              retval.begin());
+    return retval;
+  }()};  // IILE
+
+  results.enforce_exactly_equal(result, expected);
+
+  return results.print_and_return();
+}
+
---
+++ cpp/tst/src/core/fake_ranges/iota/range_for.cpp
@@ -0,0 +1,23 @@
+#include <supl/fake_ranges.hpp>
+#include <supl/test_results.hpp>
+
+auto main() -> int
+{
+  supl::test_results results;
+
+  constexpr static std::array<std::size_t, 10> expected {
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  constexpr static auto result {[]() {
+    std::array<std::size_t, 10> retval {};
+    for ( const std::size_t i : supl::fr::iota<std::size_t> {1, 11} ) {
+      retval.at(i - 1) = i;
+    }
+    return retval;
+  }()};  // IILE
+
+  results.enforce_exactly_equal(result, expected);
+
+  return results.print_and_return();
+}
+

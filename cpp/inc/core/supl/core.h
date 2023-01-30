c3bf080cbf068da0d655f4171a2c036e25e36615
theomegacarrot <theomegacarrot@gmail.com>
2023-01-30 09:43:18 -0500

    add core.h and testing.h - includes whole supple module

---
+++ cpp/inc/core/supl/core.h
@@ -0,0 +1,17 @@
+#ifndef SUPPLE_CORE_CORE_H
+#define SUPPLE_CORE_CORE_H
+
+#include "algorithm.hpp"
+#include "crtp.hpp"
+#include "fake_ranges.hpp"
+#include "functional.hpp"
+#include "invoke.hpp"
+#include "iterators.hpp"
+#include "metaprogramming.hpp"
+#include "predicates.hpp"
+#include "term_colors.h"
+#include "tuple_algo.hpp"
+#include "type_list.hpp"
+#include "utility.hpp"
+
+#endif
---
+++ cpp/inc/testing/supl/testing.h
@@ -0,0 +1,9 @@
+#ifndef SUPL_TESTING_TESTING_H
+#define SUPL_TESTING_TESTING_H
+
+#include "exception_safety.hpp"
+#include "test_results.hpp"
+#include "test_runner.hpp"
+#include "test_section.hpp"
+
+#endif

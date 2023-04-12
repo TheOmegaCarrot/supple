1d9b5c3760cd988390adc185e3cb143f418baea7
TheOmegaCarrot <theomegacarrot@gmail.com>
2023-04-12 12:29:47 -0400

    test tuple::for_each_both

--- cpp/tests/src/core/tuple_algo/for_each_both.cpp
+++ cpp/tests/src/core/tuple_algo/for_each_both.cpp
@@ -36,11 +36,42 @@ auto equal_size() -> supl::test_results
   return results;
 }
 
+auto unequal_size() -> supl::test_results
+{
+  supl::test_results results;
+
+  std::tuple tup1 {1, 'g', true};
+  std::tuple tup2 {8UL, 42U, nullptr, '^'};
+
+  const overload check_fn {[&results](int a, unsigned long b) {
+                             results.enforce_equal(a, 1);
+                             results.enforce_equal(b, 8UL);
+                           },
+                           [&results](char a, unsigned b) {
+                             results.enforce_equal(a, 'g');
+                             results.enforce_equal(b, 42U);
+                           },
+                           [&results](bool a, std::nullptr_t b) {
+                             results.enforce_true(a);
+                             results.enforce_equal(b, nullptr);
+                           }};
+
+  supl::tuple::for_each_both(tup1, tup2, check_fn);
+
+  std::tuple tup3 {1, 'g', true, '^'};
+  std::tuple tup4 {8UL, 42U, nullptr};
+
+  supl::tuple::for_each_both(tup3, tup4, check_fn);
+
+  return results;
+}
+
 auto for_each_both() -> supl::test_section
 {
   supl::test_section section;
 
   section.add_test("equal size", &equal_size);
+  section.add_test("unequal size", &unequal_size);
 
   return section;
 }

c6659d00aa3416cc62086575cdd4068e850327d0
TheOmegaCarrot <theomegacarrot@gmail.com>
2023-06-07 20:22:52 -0400

    tiny change

--- cpp/tests/src/core/tuple_algo/insert/multiple_values.cpp
+++ cpp/tests/src/core/tuple_algo/insert/multiple_values.cpp
@@ -8,9 +8,9 @@ auto main() -> int
   const std::string a_string {"neat"};
   const std::vector a_vector {1, 2, 3, 4};
 
-  const std::tuple expected_output {3, 42069, a_string, a_vector, 3.14};
+  const std::tuple expected_output {3, 81, a_string, a_vector, 3.14};
   const auto actual_output {
-    supl::tuple::insert<1>(test_input, 42069, a_string, a_vector)};
+    supl::tuple::insert<1>(test_input, 81, a_string, a_vector)};
 
   results.enforce_exactly_equal(actual_output, expected_output);
 

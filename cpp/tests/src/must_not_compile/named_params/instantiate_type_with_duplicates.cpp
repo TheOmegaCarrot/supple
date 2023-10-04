641a7c921711a9336df12f762746a159b0df34e3
TheOmegaCarrot <theomegacarrot@gmail.com>
2023-10-04 14:34:08 -0400

    add example - needs more testing compile time failures

--- cpp/tests/src/must_not_compile/named_params/instantiate_type_with_duplicates.cpp
+++ cpp/tests/src/must_not_compile/named_params/instantiate_type_with_duplicates.cpp
@@ -16,5 +16,8 @@ struct foo_count {
 
 auto main() -> int
 {
-  
+  // this should not compile, because `foo_type` is specified twice
+  [[maybe_unused]] supl::
+    named_params<foo_count, foo_type, foo_size, foo_type>
+      params {};
 }
--- cpp/tests/src/named_params/named_params.cpp
+++ cpp/tests/src/named_params/named_params.cpp
@@ -6,16 +6,22 @@
 #include "supl/test_results.hpp"
 #include "supl/test_runner.hpp"
 
+///////////// BEGIN EXAMPLE
+
+// named parameters must be a simple struct with a public `value` member...
 struct foo_size {
   std::size_t value;
 };
 
+// ...or an enum
 enum struct foo_type : char {
   foo,
   bar,
   third
 };
 
+// this `to_stream` function is just for internal usage in this test file
+// and is not necessary to add for named parameters
 template <>
 void supl::to_stream<foo_type>(std::ostream& out,
                                const foo_type& value) noexcept
@@ -33,9 +39,95 @@ void supl::to_stream<foo_type>(std::ostream& out,
   }
 }
 
+// again, a simple "value struct" is all that is needed
 struct foo_count {
   std::size_t value;
 };
+
+// just a return type for the example below
+struct foo_config {
+  foo_type type;
+  std::size_t size;
+  std::size_t count;
+
+  constexpr friend auto operator==(const foo_config& lhs,
+                                   const foo_config& rhs) noexcept
+  {
+    return (lhs.type == rhs.type) && (lhs.size == rhs.size)
+        && (lhs.count == rhs.count);
+  }
+
+  constexpr friend auto operator!=(const foo_config& lhs,
+                                   const foo_config& rhs) noexcept
+  {
+    return ! (lhs == rhs);
+  }
+
+  void to_stream(std::ostream& out) const noexcept
+  {
+    out << supl::stream_adapter {std::tie(type, size, count)};
+  }
+};
+
+// functions with named parameters must have a template type pack
+template <typename... Named_Params>
+static auto configure_foo(Named_Params&&... raw_named_params)
+{
+  // provide set of legal named parameter types
+  //
+  // the order of the types is irrelevant
+  // -
+  // I could put these types in any order
+  // without changing the behavior
+  supl::named_params<foo_type, foo_size, foo_count> named_parameters {
+    // just forward the raw named parameters in
+    std::forward<Named_Params>(raw_named_params)...};
+
+  return foo_config {
+    // get_or will get the `foo_type` parameter that was passed,
+    // or the fallback value if none was provided
+    // here, if no `foo_type` was specified, `foo_type::bar` will be used
+    named_parameters.get_or<foo_type>(foo_type::bar),
+    // pass in the underlying value type as the fallback
+    named_parameters.get_or<foo_size>(5),
+    named_parameters.get_or<foo_count>(42)};
+}
+
+// This test also demonstrates the behavior of named parameters at the call site
+// usage of the supple test framework should be easy to understand
+// (if this example is not clear, let me know!)
+static auto test_example() -> supl::test_results
+{
+  // just a test results object for the test framework
+  supl::test_results results;
+
+  results.enforce_equal(
+    // returned struct from this...
+    configure_foo(foo_type::foo, foo_size {10}, foo_count {12}),
+    // ...must equal this (type, size, count)
+    foo_config {foo_type::foo, 10, 12});
+
+  results.enforce_equal(
+    // named parameters may be in any order...
+    configure_foo(foo_size {10}, foo_type::foo, foo_count {12}),
+    // ...and the result will be the same
+    foo_config {foo_type::foo, 10, 12});
+
+  results.enforce_equal(
+    // named parameters may be omitted, if the function permits it...
+    configure_foo(),
+    // ...in this case, the function has default values for omitted parameters
+    foo_config {foo_type::bar, 5, 42});
+
+  results.enforce_equal(
+    // you can also only specify some, and again, order does not matter!
+    configure_foo(foo_size {100}),
+    foo_config {foo_type::bar, 100, 42});
+
+  return results;
+}
+
+///////////// END EXAMPLE
 
 static auto test_construction() -> supl::test_results
 {
@@ -144,6 +236,7 @@ auto test_named_params() -> supl::test_section
 {
   supl::test_section section;
 
+  section.add_test("example function", &test_example);
   section.add_test("named_params construction", &test_construction);
   section.add_test("named_params::was_passed", &test_was_passed);
   section.add_test("named_params::get", &test_get);

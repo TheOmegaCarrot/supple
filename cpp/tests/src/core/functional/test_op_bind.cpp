3fb1ec39b5132ce71e9fb95d9a01d7f5f021c33b
Ethan Hancock <ethanhancock@me.com>
2024-10-15 18:54:06 -0400

    test op_bind_{left,right}

--- cpp/tests/src/core/functional/test_op_bind.cpp
+++ cpp/tests/src/core/functional/test_op_bind.cpp
@@ -1,6 +1,54 @@
 #include "supl/functional.hpp"
 
+#include "supl/test_results.hpp"
+#include "supl/test_runner.hpp"
+#include "supl/test_section.hpp"
+
+auto test_op_bind_left() -> supl::test_results
+{
+  supl::test_results results;
+
+  auto five_plus {supl::op_bind_left<5, std::plus> {}};
+
+  results.enforce_equal(five_plus(3), 8);
+
+  auto ten_minus {supl::op_bind_left<10, std::minus> {}};
+
+  results.enforce_equal(ten_minus(3), 7);
+
+  return results;
+}
+
+auto test_op_bind_right() -> supl::test_results
+{
+  supl::test_results results;
+
+  auto plus_5 {supl::op_bind_right<std::plus, 5> {}};
+
+  results.enforce_equal(plus_5(3), 8);
+
+  auto minus_10 {supl::op_bind_right<std::minus, 10> {}};
+
+  results.enforce_equal(minus_10(17), 7);
+
+  return results;
+}
+
+auto test_op_bind() -> supl::test_section
+{
+  supl::test_section section;
+
+  section.add_test("test_op_bind_left", &test_op_bind_left);
+  section.add_test("test_op_bind_right", &test_op_bind_right);
+
+  return section;
+}
+
 int main()
 {
-  // TODO
+  supl::test_runner runner;
+
+  runner.add_section(test_op_bind());
+
+  return runner.run();
 }
